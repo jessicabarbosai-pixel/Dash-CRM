1c263099286cb9a2321b0acb3017e2e8ab1cf644
jessicabarbosai-pixel <jessicabarbosa_i@99app.com>
2026-08-27 16:18:50 -0300

    Update config.js.js

--- config.js.js
+++ config.js.js
@@ -1,23 +1,35 @@
-// config.js
 const CONFIG = {
-    SHEET_ID: 'ID_DA_PLANILHA_FURA_FILA', // Substitua pelo ID real
-    SHEET_NAME: 'Results_CRM+Fura',
-    GOOGLE_CLIENT_ID: 'SEU_CLIENT_ID', // O mesmo que você já usou no outro dash
+    // 🔑 Credenciais Google
+    SHEET_ID: '1cdQakr3EvsKlKt2Ki0Avogm8ez_AIAEy8A_nPiOaNZ4',
+    SHEET_NAME: 'Controle',
+    GOOGLE_CLIENT_ID: '926846773138-jom868tevolali8fgibvq1m01plm8vo6.apps.googleusercontent.com',
+    SCOPES: 'https://www.googleapis.com/auth/spreadsheets.readonly',
     
-    // Mapeamento exato das colunas do Excel
+    // 📊 Índices das Colunas no Google Sheets (0 = A, 1 = B, etc)
     COLUMNS: {
-        SAIDA: 'Saída ',
-        PROJETO: 'Projeto',
-        TIPO: 'Tipo',
-        ENVIO: 'Envio',
-        CIDADES: 'Cidades',
-        CANAL: 'Canal',
-        DESC_BASE: 'Descrição Base', // COLUNA H
-        MENSAGEM: 'Mensagem',        // COLUNA I
-        ID: 'ID',                    // COLUNA J
-        OBS: 'Obs',                  // COLUNA K
-        REQUEST: 'Request',
-        CLICK: 'Click',
-        CTR: 'CTR (Dash CRM) '
+        PROJETO: 1,      // Coluna B (Locadora)
+        TIPO: 2,         // Coluna C
+        ENVIO: 3,        // Coluna D (Data)
+        CANAL: 5,        // Coluna F (Canal)
+        DESC_BASE: 7,    // Coluna H (Descrição Base / Ownership)
+        REQUEST: 11,     // Coluna L (Request / Volume)
+        CLICK: 16,       // Coluna Q (Click)
+        CTR: 18          // Coluna S (CTR)
+    },
+    
+    // 🎨 Cores do Dashboard
+    COLORS: {
+        primary: '#667eea',
+        secondary: '#764ba2',
+        success: '#48bb78',
+        warning: '#ed8936',
+        info: '#4299e1',
+        locadoras: {
+            'BR22': '#4299e1',
+            'Seteloc': '#48bb78',
+            'Movida': '#ed8936',
+            'Foco': '#f56565',
+            'Nexovia': '#9f7aea'
+        }
     }
 };
